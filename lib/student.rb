3a6d713c88d99f18e414ce7942b4d35088c231d5
Josh Raiborde <joshsrai@yahoo.com>
2020-08-31 19:57:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -75,9 +75,11 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE students.grade < ?
+      WHERE students.grade < 12;
     SQL
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
   
   
